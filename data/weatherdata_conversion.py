c54c036ba8ac670483207c9b034f5f5da266765e
Jun Terauchi <mimopapa@gmail.com>
2018-02-22 18:30:06 +0900

    add kibana document

---
+++ data/weatherdata_conversion.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 21 13:45:58 2018
+
+@author: terauchi
+"""
+
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('chiba.csv', encoding="cp932")
+
