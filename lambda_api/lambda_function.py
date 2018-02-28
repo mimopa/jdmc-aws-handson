4b3e7028b0eeae095b701c8b69a62ff5f849cb6f
Jun Terauchi <mimopapa@gmail.com>
2018-02-28 16:46:06 +0900

    modified document

---
+++ lambda_api/lambda_function.py
@@ -0,0 +1,12 @@
+import json
+
+def everypost_handler(event, context):
+    print(json.dumps(event, indent=4))
+    # JSON形式の戻り値を設定する
+    return {
+        'statusCode' : 200,
+        'headers' : {
+            'content-type' : 'text/html'
+        },
+        'body' : '<html><body>OK</body></html>'
+    }
